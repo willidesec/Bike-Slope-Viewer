88db4a71493022b23f21f07a6d5aa18f1a150d45
William Désécot <william.desecot@hotmail.fr>
2024-07-06 18:30:57 +0200

    feat: init an Xcode project

---
+++ BikeSlopeViewer/BikeSlopeViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeSlopeViewerApp.swift
+//  BikeSlopeViewer
+//
+//  Created by William DESECOT on 06/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct BikeSlopeViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BikeSlopeViewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BikeSlopeViewer
+//
+//  Created by William DESECOT on 06/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
